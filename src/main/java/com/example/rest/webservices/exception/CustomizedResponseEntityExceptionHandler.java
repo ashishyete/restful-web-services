5b3d95ff63c8cc3e2af121f737b55ff89506b2b3
ashishyete <ashish.yete@gmail.com>
2021-11-29 17:17:06 +0530

    Changed ResponseEntity return type class param

--- src/main/java/com/example/rest/webservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/rest/webservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -18,25 +18,25 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) throws Exception {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<Object>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity(exceptionResponse,HttpStatus.NOT_FOUND);
+        return new ResponseEntity<Object>(exceptionResponse,HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidInputException.class)
     public final ResponseEntity<Object> handleInvalidInputException(Exception ex, WebRequest request) throws Exception {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity(exceptionResponse,HttpStatus.NOT_ACCEPTABLE);
+        return new ResponseEntity<Object>(exceptionResponse,HttpStatus.NOT_ACCEPTABLE);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ApplicationConstants.Validation_Failed, ex.getFieldError().toString());
-        return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<Object>(exceptionResponse,HttpStatus.BAD_REQUEST);
     }
 
 }
